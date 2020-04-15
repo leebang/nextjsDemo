352ea53ed18f39672087a469b1a8c3bd002ec571
leebang <leebang31698@gmail.com>
2020-04-15 14:44:54 -0400

    css update

--- pages/index.js
+++ pages/index.js
@@ -68,8 +68,20 @@ export default function Index() {
     const theStyle = {
         height: "105vh", 
         width: "100vw",
-        background: theColor
+        background: theColor,
+        
     }
+
+    const inputStyle = {
+        textAlign: "center !important",
+    }
+
+    const inputWrapper = "ui focus input transparent"
+    const inputWrapperStyle = {width: "54.5px", right:"23px", textAlign: "center", lineHeight: "1em", background:"rgb(25,25,25,0.2)"}
+    const inputWrapperStyleHex = {width: "80px", textAlign: "center", lineHeight: "1em", background:"rgb(25,25,25,0.2)"}
+    const boxStyle = {background:"rgb(255,255,255,0.5)", width: "300px", boxShadow: "1px 3px 10px #111111", borderRadius: "5px"}
+
+
 
     const setDefault = () => {
         setRValue("")
@@ -140,23 +152,20 @@ export default function Index() {
       
       <div style={theStyle}>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-        <Grid rows={3} centered >
+        <Grid rows={3} centered>
         
         <Grid.Row style={{height:"30vh"}}></Grid.Row>
 
-        <Grid>
+        <Grid style={boxStyle}>
         <Grid.Row columns={1}  width={8} centered >
             <h2>Hex RGB Converter</h2>
         </Grid.Row>
 
-        <Grid.Row columns={1}  width={8} centered >
-        
-            <h4>Hex Value </h4>
-            {/* {theHex} */}
-            {/* <br/> */}
-            <div class="ui focus input"><input 
+        <Grid.Row columns={1}  width={8} centered>
+            <h4>Hex</h4>
+            <div class={inputWrapper} style={inputWrapperStyleHex}><input
+            style={inputStyle}
             type="text"
-            placeholder = "Enter HEX value"
             maxLength = "6"
             value = {theHex}
             onChange = {(e) => {
@@ -165,46 +174,52 @@ export default function Index() {
                 } else {
                     setDefault()
                 }
-                
             }
             }
-            />
-            </div>
+            /></div>
+            
         </Grid.Row>
 
-        <Grid.Row columns={5}  centered >
-            <Grid.Column textAlign='center'>
-                <h4 style={{color:"red"}}>Red</h4> 
+        {/* RGB part */}
+        <Grid.Row columns={5} centered style={{left:"10px"}} >
+
+            <Grid.Column centered>
+                <h4 style={{color:"red"}}>R</h4> 
                 {/* {rValue} */}
                 {/* <br/> */}
-                <div class="ui focus input"><input 
+                <div class={inputWrapper} style={inputWrapperStyle}><input 
+                style={inputStyle}
                 type="text"
-                placeholder = "Enter Red value"
                 value = {rValue}
                 maxLength = "3"
                 onChange = {(e) => {
-                    if (e.target.value) {
+                    if (e.target.value <= 255) {
                         setHEX(e.target.value, gValue, bValue)
+                    } else if (e.target.value > 255) {
+                        
                     } else {
                         setDefault()
                     }
                 }
+                
                 }
                 /></div>
             </Grid.Column>
 
-            <Grid.Column centered textAlign='center'>
-                <h4 style={{color:"green"}}>Green </h4> 
+            <Grid.Column centered>
+                <h4 style={{color:"green"} }>G</h4> 
                 {/* {gValue} */}
                 {/* <br/> */}
-                <div class="ui focus input"><input 
+                <div class={inputWrapper} style={inputWrapperStyle}><input
+                style={inputStyle}
                 type="text"
-                placeholder = "Enter Green value"
                 value = {gValue}
                 maxLength = "3"
                 onChange = {(e) => {
-                    if (e.target.value) {
+                    if (e.target.value <= 255) {
                         setHEX(rValue, e.target.value, bValue)
+                    } else if (e.target.value > 255) {
+
                     } else {
                         setDefault()
                     }
@@ -213,18 +228,20 @@ export default function Index() {
                 /></div>
             </Grid.Column>
 
-            <Grid.Column centered textAlign='center'>
-                <h4 style={{color:"blue"}}>Blue</h4> 
+            <Grid.Column centered>
+                <h4 style={{color:"blue"}}>B</h4> 
                 {/* {bValue} */}
                 {/* <br/> */}
-                <div class="ui focus input"><input 
+                <div class={inputWrapper} style={inputWrapperStyle}><input 
+                style={inputStyle}
                 type="text"
-                placeholder = "Enter Blue value"
                 value = {bValue}
                 maxLength = "3"
                 onChange = {(e) => {
-                    if (e.target.value) {
+                    if (e.target.value <= 255) {
                         setHEX(rValue, gValue, e.target.value)
+                    } else if (e.target.value > 255) {
+                        
                     } else {
                         setDefault()
                     }

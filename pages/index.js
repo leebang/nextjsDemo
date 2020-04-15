61ca959898dcc4a73fbb48b16130cbd086b63111
leebang <leebang31698@gmail.com>
2020-04-14 20:05:22 -0400

    small fix

--- pages/index.js
+++ pages/index.js
@@ -128,8 +128,6 @@ export default function Index() {
       
       <div style={theStyle}>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-        {router.query.hex}
-
 
         <Grid rows={3} centered >
         
